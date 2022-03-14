e1dbcf57383540dbb3009e0245abfa9e0c39c213
duraznoj <sashka.warner@gmail.com>
2022-03-14 00:27:27 -0700

    Add messages, checking guess, colors, iteration

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
 const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
 
-const tileDisplay = document.querySelector('.tile-container')
+const tileDisplay = document.querySelector('.tile-container');
+const messageDisplay = document.querySelector('.message-container');
 const keys = document.querySelectorAll('.key');
 const whiteKeys = document.querySelectorAll('.key.white'); //have to add period for space
 const blackKeys = document.querySelectorAll('.key.black');
 const buttons = document.querySelectorAll('.item-4-piano button');
+let isGameOver = false;
 
-const musicle = ['C', 'D', 'E', 'D', 'Eb']
+const musicle = 'CDEFEb'
 
 const VF = Vex.Flow;
-const numNotes = 2;
 const contextArr = [];
-let numClicks = 0;
+/*let numClicks = 0;*/
+let currentRow = 0;
+let currentTile = 0;
+
 
 const guessRows = [
   ['', '', '', '', ''],
@@ -21,10 +25,19 @@ const guessRows = [
   ['', '', '', '', ''],
   ['', '', '', '', ''],
   ['', '', '', '', '']
-]
+];
+
+const contextRows = [
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', '']
+];
 
 /*boilerplate VF code - note: has to be before the guessrows for loop*/
-const createContext = (divID) => {
+const createContext = (divID, hasClef) => {
   /*const VF = Vex.Flow;*/
   /*const div = document.getElementById(divID)*/
   const div = document.querySelector(divID)
@@ -35,7 +48,7 @@ const createContext = (divID) => {
   /*add stave*/
   const stave = new VF.Stave(10, 0, 300).addClef('treble');
   stave.setContext(context).draw();
-  return [context, stave];
+  return [context, stave]; /*store context and stave objects so we can have something to draw notes on later*/
 }
 
 guessRows.forEach((guessRow, guessRowIndex) => {
@@ -50,51 +63,97 @@ guessRows.forEach((guessRow, guessRowIndex) => {
   tileDisplay.append(rowElement);
 
   guessRow.forEach((guess, guessIndex) => {
-    contextArr.push(createContext('#guessRow-' + guessRowIndex + '-tile-' + guessIndex)); /*need the id identifier*/
+    /*contextArr.push(createContext('#guessRow-' + guessRowIndex + '-tile-' + guessIndex)); /*need the id identifier*/
+    contextRows[guessRowIndex][guessIndex] = createContext('#guessRow-' + guessRowIndex + '-tile-' + guessIndex);
   });
 });
 
+/*console.table(contextRows);*/
+
+const showMessage = (message) => {
+  const messageElement = document.createElement('p');
+  messageElement.textContent = message;
+  messageDisplay.append(messageElement);
+  setTimeout(() => messageDisplay.removeChild(messageElement), 2000);
+
+}
+
+const flipTile = () => {
+  const rowTiles = document.querySelector("#guessRow-" + currentRow).childNodes;
+  rowTiles.forEach((tile, index) => {
+    const dataNote = tile.getAttribute('data');
+    if(dataNote == musicle[index]){
+      tile.classList.add('green-overlay');
+    } else if(musicle.includes(dataNote)){  /*to-do - decide on convention for note naming*/
+      tile.classList.add('yellow-overlay');
+    } else {
+      tile.classList.add('grey-overlay');
+    }
+  });
+};
+
 keys.forEach(key => {
-  key.addEventListener('click', () => playNote(key))
+  key.addEventListener('click', () => {
+    if(currentTile < 5 && currentRow < 6){
+      playNote(key);
+    }
+  });
 });
 
 buttons.forEach((button) =>{
   button.addEventListener('click', () => editNote(button));
-})
+});
+
+
 
 const editNote = (button) => {
   /*console.log('clicked');*/
   /*document.getElementsByClassName("buttons")[0].id; */
   console.log(button.id)
-}
-/*document.addEventListener('keydown', (e) => {
-  if(e.repeat) return
-  const key = e.key;
-  const whiteKeyIndex = WHITE_KEYS.indexOf(key);
-  const blackKeyIndex = BLACK_KEYS.indexOf(key);
+  if(button.id == "delete" && currentTile > 0){
+    currentTile--; /*go back to previous tile*/
+    const context = contextRows[currentRow][currentTile][0]; /*get context - consider making this a single value for all functions?*/
+    context.svg.removeChild(context.svg.lastChild); /* delete note from stave*/
+    guessRows[currentRow][currentTile] = ''; /*delete note from matrix*/
+    console.log(guessRows);
+  }
+  else if(button.id == "enter" && currentTile > 4){
 
-  if (whiteKeyIndex > -1)
-  {
-    playNote(whiteKeys[whiteKeyIndex]);
+    const guess = guessRows[currentRow].join('');
+    console.log('guess = ' + guess + "musicle= " + musicle);
+
+    flipTile();
+
+    if(guess == musicle){
+      showMessage("Outstanding!");
+      isGameOver == true;
+      return;
+    } else if(currentRow >= 5){
+      showMessage("Game over.");
+      isGameOver = true;
+      return;
+    } else if(currentRow < 5){
+      currentRow++;
+      currentTile = 0;
+    }
   }
-  if(blackKeyIndex > -1){
-    playNote(blackKeys[blackKeyIndex]);
-  }
-})*/
+}
 
 function playNote(key){
-  /*var attribs = key.attributes;
-        for(var i = 0; i < attribs.length; i++) {
-            console.log(attribs[i]);
-        }*/
-
-  /*console.log("key: " + key.dataset.note);*/
-
-  numClicks += 1;
-  console.log(numClicks);
+  /*numClicks += 1;
+  console.log(numClicks);*/
 
   var currentNote = key.dataset.note;
   console.log("key: " + currentNote);
+  console.log(guessRows);
+
+  /*store note in guessRows matrix*/
+  guessRows[currentRow][currentTile] = currentNote;
+
+  /*add note value to element data value*/
+  const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile);
+  tile.setAttribute('data', currentNote);
+
   const noteAudio = document.getElementById(key.dataset.note);
   //console.log("key: "+noteAudio);
   noteAudio.currentTime = 0;
@@ -103,16 +162,15 @@ function playNote(key){
   noteAudio.addEventListener('ended', () => {
     key.classList.remove('active');
   })
-  /* to do - add logic for listening for sequence of notes then rendering at end...
-  but also want to have feedback for user as to which notes have been enterered*/
-  var notes = [
+
+  var stave_note = [
     // A quarter-note.
     new VF.StaveNote({clef: "treble", keys: [currentNote + "/4"], duration: "q" }).setStem(new VF.Stem()),
   ];
 
   // Create a voice in 4/4 and add the note from above
   var voice = new VF.Voice({num_beats: 1,  beat_value: 4});
-  voice.addTickables(notes);
+  voice.addTickables(stave_note);
 
   // Format and justify the notes to 350 pixels (50 pixels left for key and time signatures).
   var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 350);
@@ -122,5 +180,9 @@ function playNote(key){
   /*console.table(contextArr[1]);*/ /*undefined*/
   /*console.table(document.querySelector("svg"));
   console.table(document.querySelector(".vf-stave"));*/
-  voice.draw(contextArr[numClicks-1][0], contextArr[numClicks-1][1]);
+  /*voice.draw(contextArr[numClicks-1][0], contextArr[numClicks-1][1]);*/
+  voice.draw(contextRows[currentRow][currentTile][0], contextRows[currentRow][currentTile][1]); /* args = [context, stave]*/
+  
+  currentTile++;
 }
+
