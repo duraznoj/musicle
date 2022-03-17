d8a3f7a1a269fb6c0e32f2745ffc10a576ad9a49
duraznoj <sashka.warner@gmail.com>
2022-03-17 00:12:36 -0700

    Add dynamic html, lookup table, guess checking
    Add generation of html piano and audio elements via javascript. Add
    lookup table with pitch, note name and key color data that can be
    referenced by pitch as an index. Add conversion of midi pitch numbers
    to letters for use in arguments to vexflow. Fix guess checking and
    guess/melody type mismatch issues.

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
-const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+/*CONSTANTS*/
+/*const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];*/ /*only need this for keyboard input*/
 
 /*select range of pitches we want to use for the piano keys*/
 const pitches = [];
 const lowest_pitch = 48;
 const highest_pitch = 71;
-
-/*initialize blank pitch to note name conversion table object */
-var conversion_table = {};
 
 /*create corresponding array for note names for use with vexflow*/
 const note_letters = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
@@ -15,16 +13,24 @@ const note_names_lower = note_letters.map((val) => val + '/3');
 const note_names_higher = note_letters.map((val) => val + '/4');
 const note_names = note_names_lower.concat(note_names_higher);
 
+/*create array of key colors for use in generating piano*/
+const key_colors_basic = ["white", "black", "white", "black", "white", "white", "black", "white", "black", "white", "black", "white"]
+const key_colors = key_colors_basic.concat(key_colors_basic)
+
 /*define constants for accessing and creating html elements*/
+const body = document.querySelector('body');
 const tileDisplay = document.querySelector('.tile-container');
 const messageDisplay = document.querySelector('.message-container');
-const keys = document.querySelectorAll('.key');
-const whiteKeys = document.querySelectorAll('.key.white'); //have to add period for space
-const blackKeys = document.querySelectorAll('.key.black');
-const buttons = document.querySelectorAll('.item-4-piano button');
+const piano = document.querySelector('.item-4-piano')
+
+/*boolean for game status*/
 let isGameOver = false;
 
-const musicle = 'CDEFEb'
+/*choose melody for the game*/
+const musicle_in = [48,49,50,51,52];
+/*convert melody array of numbers to an array of strings for comparing with div attributes later on*/
+let musicle = musicle_in.map(val => val.toString());
+/*console.log(musicle)*/
 
 const VF = Vex.Flow;
 const contextArr = [];
@@ -49,22 +55,69 @@ const contextRows = [
   ['', '', '', '', '']
 ];
 
+/*MAIN*/
 /*populate pitches array for use in generating piano keys*/
 for(let p = lowest_pitch; p <= highest_pitch; p++){
   pitches.push(p);
 };
 
-/*create lookup table from the pitches and the vexflow note names for converting between the two*/
-pitches.forEach((pitch, index) => conversion_table[pitch] = note_names[index]);
+/*create lookup table from the pitches, vexflow note names and piano key colors for converting between them*/
+let conversion_lookup = {};
+for (let i=0; i < pitches.length; i++) {
+  /*populate conversion_lookup object indices with the elements of pitches, and assign data to the indices from the pitches, note_names, and key_colors arrays
+  can concat multiple variables for the index if we want more details*/
+  conversion_lookup[pitches[i]] = {"pitch" : pitches[i],"note_name" : note_names[i],"key_color" : key_colors[i]}; 
+}
+/*console.log(conversion_lookup[48].key_color)*/
+
+/*console.table(typeof(conversion_lookup[48].pitch))*/
 
 /*function to convert between midi pitch number and vexflow note names*/
-const convert_pitches = (input_pitches) => {
-  let output_notes = [];
-  input_pitches.forEach((pitch, index) => {
-    output_notes.push(conversion_table[pitch]);
+const convert_pitches = (in_pitches) => {
+  let out_notes = [];
+  in_pitches.forEach((pitch) => {
+    out_notes.push(conversion_lookup[pitch].note_name);
   });
-  return(output_notes);
+  return(out_notes);
 };
+
+/*create elements for list of note audio element*/
+pitches.forEach((pitch, index) => {
+  const audio_element = document.createElement('audio');
+  audio_element.setAttribute('id', pitch);
+  audio_element.src = "notes/notes_" + pitch + ".wav";
+  body.append(audio_element);
+});
+
+/*create enter button*/
+const enter_button = document.createElement('button');
+enter_button.setAttribute('id', 'enter');
+enter_button.textContent = 'Enter';
+piano.append(enter_button);
+
+/*create html elements to represent keys for piano and append relevant properties from lookup table*/
+pitches.forEach((pitch, index) => {
+  const key_element = document.createElement('div');
+  key_element.classList.add("key", conversion_lookup[pitch].key_color); /*comma used to separate multiple classes*/
+  key_element.setAttribute('data-note', pitch);
+  piano.append(key_element);
+});
+
+/*create delete button*/
+const delete_button = document.createElement('button');
+delete_button.setAttribute('id', 'delete');
+delete_button.textContent = 'Delete';
+piano.append(delete_button);
+
+
+/*get newly created elements*/
+const keys = document.querySelectorAll('.key');
+const whiteKeys = document.querySelectorAll('.key.white'); //have to add period for space
+const blackKeys = document.querySelectorAll('.key.black');
+const buttons = document.querySelectorAll('.item-4-piano button');
+
+/*console.table(keys)*/
+
 
 /*boilerplate VF code - note: has to be before the guessrows for loop*/
 const createContext = (divID, hasClef) => {
@@ -80,11 +133,6 @@ const createContext = (divID, hasClef) => {
   stave.setContext(context).draw();
   return [context, stave]; /*store context and stave objects so we can have something to draw notes on later*/
 }
-
-pitches.forEach((pitch) => {
-  const keyElement = document.createElement('div');
-  /*keyElement.setAttribute('') to do- generate function to assign color of key*/
-});
 
 /*create tiles for music notes*/
 guessRows.forEach((guessRow, guessRowIndex) => {
@@ -108,6 +156,7 @@ guessRows.forEach((guessRow, guessRowIndex) => {
 
 /*console.table(contextRows);*/
 
+/*function to show messages after guesses and at end of game*/
 const showMessage = (message) => {
   const messageElement = document.createElement('p');
   messageElement.textContent = message;
@@ -116,13 +165,14 @@ const showMessage = (message) => {
 
 }
 
+/*function to color tiles based on guesses*/
 const flipTile = () => {
   const rowTiles = document.querySelector("#guessRow-" + currentRow).childNodes;
   rowTiles.forEach((tile, index) => {
     const dataNote = tile.getAttribute('data');
     if(dataNote == musicle[index]){
       tile.classList.add('green-overlay');
-    } else if(musicle.includes(dataNote)){  /*to-do - decide on convention for note naming*/
+    } else if(musicle.includes(dataNote)){
       tile.classList.add('yellow-overlay');
     } else {
       tile.classList.add('grey-overlay');
@@ -134,6 +184,7 @@ keys.forEach(key => {
   key.addEventListener('click', () => {
     if(currentTile < 5 && currentRow < 6){
       playNote(key);
+      /*console.table(guessRows)*/
     }
   });
 });
@@ -143,7 +194,7 @@ buttons.forEach((button) =>{
 });
 
 
-
+/*function to handle what happens when you click the enter or delete buttons*/
 const editNote = (button) => {
   /*console.log('clicked');*/
   /*document.getElementsByClassName("buttons")[0].id; */
@@ -155,14 +206,18 @@ const editNote = (button) => {
     guessRows[currentRow][currentTile] = ''; /*delete note from matrix*/
     console.log(guessRows);
   }
+  /*if the enter button is pressed and 5 tiles have been populated then check if the guess is correct*/
   else if(button.id == "enter" && currentTile > 4){
 
     const guess = guessRows[currentRow].join('');
-    console.log('guess = ' + guess + "musicle= " + musicle);
+    const musicle_join = musicle.join('');
+    console.log('guess = ' + guess + " musicle= " + musicle_join);
+    /*console.log(guess === musicle_join)*/
 
+    /*color tile based on guess accuracy using the flipTile() function*/
     flipTile();
 
-    if(guess == musicle){
+    if(guess === musicle_join){
       showMessage("Outstanding!");
       isGameOver == true;
       return;
@@ -177,13 +232,12 @@ const editNote = (button) => {
   }
 }
 
+/*function to play note audio, store note in guessRows matrix, and render note on vexflow staves in the tile elements*/
 function playNote(key){
-  /*numClicks += 1;
-  console.log(numClicks);*/
 
   var currentNote = key.dataset.note;
   console.log("key: " + currentNote);
-  console.log(guessRows);
+  /*console.log(guessRows);*/
 
   /*store note in guessRows matrix*/
   guessRows[currentRow][currentTile] = currentNote;
@@ -201,9 +255,10 @@ function playNote(key){
     key.classList.remove('active');
   })
 
-  /*var stave_note = [
+  var stave_note = [
     // A quarter-note.
-    new VF.StaveNote({clef: "treble", keys: [currentNote + "/4"], duration: "q" }).setStem(new VF.Stem()),
+    /*new VF.StaveNote({clef: "treble", keys: [currentNote + "/4"], duration: "q" }).setStem(new VF.Stem()),*/ /*only use if input notes are letters*/
+    new VF.StaveNote({clef: "treble", keys: [conversion_lookup[currentNote].note_name + "/4"], duration: "q" }).setStem(new VF.Stem()),
   ];
 
   // Create a voice in 4/4 and add the note from above
@@ -219,7 +274,7 @@ function playNote(key){
   /*console.table(document.querySelector("svg"));
   console.table(document.querySelector(".vf-stave"));*/
   /*voice.draw(contextArr[numClicks-1][0], contextArr[numClicks-1][1]);*/
-  /*voice.draw(contextRows[currentRow][currentTile][0], contextRows[currentRow][currentTile][1]); /* args = [context, stave]*/
+  voice.draw(contextRows[currentRow][currentTile][0], contextRows[currentRow][currentTile][1]); /* args = [context, stave]*/
   
   currentTile++;
 }
