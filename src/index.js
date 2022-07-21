c6d36feb58bd5b63c4b3b8449261439a4efe8a25
duraznoj <sashka.warner@gmail.com>
2022-07-21 16:29:24 -0700

    Stop playing audio for mobile testing

--- src/index.js
+++ src/index.js
@@ -803,7 +803,7 @@ function playNote(key){
 
   const noteAudio = document.getElementById(key.dataset.note);
   noteAudio.currentTime = 0;
-  noteAudio.play();
+  //noteAudio.play();
   key.classList.add('active');
   noteAudio.addEventListener('ended', () => {
     key.classList.remove('active');
